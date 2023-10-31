aefdd3f613e19aabc70dd6e982c0f9c2230111a4
Eclarkhalid <eclarkhalid@gmail.com>
2023-11-01 00:12:25 +0300

    updating margin

--- pages/components/Form.js
+++ pages/components/Form.js
@@ -10,27 +10,27 @@ export default function Form() {
         <form action="" class="space-y-5">
           <div class="grid grid-cols-12 gap-5">
             <div class="col-span-6">
-              <label for="example7" class="mb-1 block text-sm font-medium text-gray-700">Email</label>
+              <label for="example7" class="mb-1 block text-sm font-medium text-text">Email</label>
               <input type="email" id="example7" class="block w-full rounded-md p-3 border border-gray-300 shadow-sm focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500" placeholder={session.user.email} />
             </div>
             <div class="col-span-6">
-              <label for="example8" class="mb-1 block text-sm font-medium text-gray-700">Full Name</label>
+              <label for="example8" class="mb-1 block text-sm font-medium text-text">Full Name</label>
               <input type="text" id="example8" class="block w-full rounded-md p-3 border border-gray-300 shadow-sm focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500" placeholder={session.user.name} />
             </div>
             <div class="col-span-12">
-              <label for="example9" class="mb-1 block text-sm font-medium text-gray-700">Address</label>
+              <label for="example9" class="mb-1 block text-sm font-medium text-text">Address</label>
               <input type="text" id="example9" class="block w-full rounded-md p-3 border border-gray-300 shadow-sm focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500" placeholder="1864 Main Street" />
             </div>
             <div class="col-span-6">
-              <label for="example10" class="mb-1 block text-sm font-medium text-gray-700">City</label>
+              <label for="example10" class="mb-1 block text-sm font-medium text-text">City</label>
               <input type="text" id="example10" class="block w-full rounded-md p-3 border border-gray-300 shadow-sm focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500" placeholder="" />
             </div>
             <div class="col-span-4">
-              <label for="example11" class="mb-1 block text-sm font-medium text-gray-700">State</label>
+              <label for="example11" class="mb-1 block text-sm font-medium text-text">State</label>
               <input type="text" id="example12" class="block w-full rounded-md p-3 border border-gray-300 shadow-sm focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500" placeholder="" />
             </div>
             <div class="col-span-2">
-              <label for="example12" class="mb-1 block text-sm font-medium text-gray-700">Zip</label>
+              <label for="example12" class="mb-1 block text-sm font-medium text-text">Zip</label>
               <input type="text" id="example12" class="block w-full rounded-md p-3 border border-gray-300 shadow-sm focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500" placeholder="" />
             </div>
             <div class="col-span-12 text-center">
