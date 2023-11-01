7ed2ca99eddb3f3fc0976cef336682e117ba0ab0
Eclarkhalid <eclarkhalid@gmail.com>
2023-11-01 08:42:13 +0300

    updating buttons

--- pages/products/index.js
+++ pages/products/index.js
@@ -26,12 +26,12 @@ export default function Products({ allProducts }) {
       {loading ? (
         <Spinner />
       ) : (
-        <div className="mt-14 md:mt-6 grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-8 px-2">
+        <div className="mt-14 md:mt-6 grid grid-cols-2 gap-x-3 md:gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-8 px-2">
           {allProducts.map((product) => (
             <div key={product._id}>
               <div className="group block overflow-hidden border border-accent rounded-xl border-opacity-10">
                 <div className="p-1">
-                  <div className="relative h-[300px] sm:h-[300px]">
+                  <div className="relative md:h-[300px] h-[200px]">
                     <img
                       src={product.images[0]}
                       alt=""
@@ -51,7 +51,7 @@ export default function Products({ allProducts }) {
                       </h3>
                     </Link>
 
-                    <div className="mt-1.5 flex items-center justify-between text-text">
+                    <div className="mt-1.5 flex flex-col md:flex-row items-center justify-between text-text">
                       <p className="tracking-wide text-primary">ksh. {formatPrice(product.price)}</p>
 
                       <button onClick={() => addProduct(product._id)} type="button" className="flex items-center divide-x rounded-lg border border-primary bg-white text-center text-md font-medium text-secondary-700 shadow-sm hover:bg-gray-100">
@@ -59,7 +59,7 @@ export default function Products({ allProducts }) {
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                           </svg>
-                          <span>Add</span>
+                          <span className="">Add</span>
                         </div>
                       </button>
                     </div>
