d4aca31d61c418d01abcc212c96173e13ce3f886
Eclarkhalid <eclarkhalid@gmail.com>
2023-11-01 09:07:17 +0300

    styling buttons

--- pages/products/index.js
+++ pages/products/index.js
@@ -51,17 +51,27 @@ export default function Products({ allProducts }) {
                       </h3>
                     </Link>
 
-                    <div className="mt-1.5 flex  items-center justify-between text-text">
+                    <div className="mt-1.5 flex flex-col md:flex-row  items-center justify-between text-text">
                       <p className="tracking-wide text-primary text-sm md:text-md">ksh. {formatPrice(product.price)}</p>
 
-                      <button onClick={() => addProduct(product._id)} type="button" className="flex items-center divide-x rounded-lg border border-primary bg-white text-center text-md font-medium text-secondary-700 shadow-sm hover:bg-gray-100">
-                        <div className="flex items-center space-x-2 py-2.5 px-3">
-                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-                          </svg>
-                          <span className="hidden md:flex">Add</span>
-                        </div>
-                      </button>
+                      <div className="mt-3 md:mt-0 col-span-12 hidden md:flex">
+                        <button onClick={() => addProduct(product._id)} type="button" className="flex items-center divide-x rounded-lg border border-primary bg-white text-center text-md font-medium text-secondary-700 shadow-sm hover:bg-gray-100">
+                          <div className="flex items-center space-x-2 py-2.5 px-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                            </svg>
+                            <span>Add</span>
+                          </div>
+                        </button>
+                      </div>
+                      <div class="col-span-12 text-center w-full mt-3 md:mt-0 md:hidden">
+                        <button
+                          onClick={() => addProduct(product._id)}
+                          className="disabled block rounded bg-secondary px-5 py-3 text-md text-text w-full transition hover:bg-purple-300"
+                        >
+                          Add to cart
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -73,16 +83,16 @@ export default function Products({ allProducts }) {
     </div>
   );
 
-  }
+}
 
 
-  export async function getServerSideProps() {
-    await mongooseConnect();
-    const allProducts = await Product.find({}, null, { sort: { '_id': 1 } })
+export async function getServerSideProps() {
+  await mongooseConnect();
+  const allProducts = await Product.find({}, null, { sort: { '_id': 1 } })
 
-    return {
-      props: {
-        allProducts: JSON.parse(JSON.stringify(allProducts))
-      },
-    };
-  }
+  return {
+    props: {
+      allProducts: JSON.parse(JSON.stringify(allProducts))
+    },
+  };
+}
